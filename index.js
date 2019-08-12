1aa19f5b4f8d346a451640af326c8f37bed72247
Fernando López Guevara <fernando.lguevara@gmail.com>
2019-08-12 14:23:20 -0300

    use baseUrl option to generate maps

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const fromPairs = pairs => pairs.reduce((res, [key, value]) => ({ ...res, [key]: value }), {})
+const fromPairs = pairs =>
+  pairs.reduce((res, [key, value]) => ({ ...res, [key]: value }), {});
 
 /**
  * tsconfig の paths の設定から moduleNameMapper を生成する
  * {"@app/*": ["src/*"]} -> {"@app/(.*)": "<rootDir>/src/$1"}
  */
-module.exports = function moduleNameMapperFromTSPaths(tsconfig) {
+module.exports = function moduleNameMapperFromTSPaths({
+  compilerOptions: { baseUrl, paths },
+}) {
+  baseUrl = baseUrl ? `${baseUrl}/` : '';
+  
   return fromPairs(
-    Object.entries(tsconfig.compilerOptions.paths).map(([k, [v]]) => [
-      `^${k.replace(/\*/, "(.*)")}`,
-      `<rootDir>/${v.replace(/\*/, "$1")}`,
+    Object.entries(paths).map(([k, [v]]) => [
+      `^${k.replace(/\*/, '(.*)')}`,
+      `<rootDir>/${baseUrl}${v.replace(/\*/, '$1')}`,
     ]),
-  )
-}
+  );
+};
