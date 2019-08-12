9aba57c42518985ee543eade683ff992899edfea
Fernando López Guevara <fernando.lguevara@gmail.com>
2019-08-12 14:22:31 -0300

    use expect instead of assert

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,8 @@
-import assert from "assert"
 import config from "../jest.config.js"
 import Mock from "@test/mock"
 
 describe("jest", () => {
   it("resolves path", () => {
-    assert(Mock != null)
+    expect(Mock).not.toBe(null);
   })
 })
